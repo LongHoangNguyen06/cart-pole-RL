48a8749b9d6407f4bc1bed06d02f1a97878ae0bf
ttchopper06 <157883314+ttchopper06@users.noreply.github.com>
2024-02-27 11:41:28 +0100

    Use specific python version

--- config/run.sh
+++ config/run.sh
@@ -1,3 +1,3 @@
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 
-screen -S ${branch_name} "python3 -m main --device cuda --mode rgb_array --train &> logs/${branch_name}.log"
+screen -S ${branch_name} "/home/long/anaconda3/envs/DL/bin/python3 -m main --device cuda --mode rgb_array --train &> logs/${branch_name}.log"
