5565887211099be327a92718393071473d7dea0b
ttchopper06 <157883314+ttchopper06@users.noreply.github.com>
2024-02-27 10:39:28 +0100

    Clip grad

--- train.py
+++ train.py
@@ -39,6 +39,8 @@ def train_iteration(net: network.Network,
     loss = ((state_action_expected_reward - state_action_reward_pred)**2).mean()
     opt.zero_grad()
     loss.backward()
+    for param in net.parameters():
+        param.grad.data.clamp_(-1, 1)
     opt.step()
     return loss
 
