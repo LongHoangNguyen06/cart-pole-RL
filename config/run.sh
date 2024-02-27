0f0f8824fc62afc523191575ddf2e6385901ff4f
ttchopper06 <157883314+ttchopper06@users.noreply.github.com>
2024-02-27 11:41:01 +0100

    Let's see if it works

--- config/run.sh
+++ config/run.sh
@@ -1,5 +1,3 @@
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
-python3 -m main \
---device cuda \
---mode rgb_array \
---train &> logs/${branch_name}.log
+
+screen -S ${branch_name} "python3 -m main --device cuda --mode rgb_array --train &> logs/${branch_name}.log"
